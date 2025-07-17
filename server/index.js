9e8ffefdc609431df27d45a91b3b28a8f1b23776
dinraj910 <dinrajdinesh564@gmail.com>
2025-07-17 21:21:03 +0530

    First Commmit

---
+++ server/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const  Team = require('./models/Team');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URI, {
+})
+.then(() => console.log("MongoDB Connected ✅✅✅"))
+.catch(err => console.error("MongoDB Connection Failed:", err));
+
+//  API 1: Check if a team already registered (GET)
+
+app.get('/api/check/:teamName', async (req, res) => {
+  const teamName = req.params.teamName.toLowerCase();
+  const team = await Team.findOne({ teamNameLower: teamName });
+
+  if (team) {
+    res.status(200).json({
+      exists: true,
+      topic: team.topic
+    });
+  } else {
+    res.status(200).json({ exists: false });
+  }
+});
+
+// API 2: Register team and selected topic (POST)
+app.post('/api/spin', async (req, res) => {
+  try {
+    const { teamName, members, topic } = req.body;
+
+    const teamNameLower = teamName.toLowerCase();
+
+    const already = await Team.findOne({ teamNameLower });
+    if (already) {
+      return res.status(409).json({
+        message: "Team already registered",
+        topic: already.topic
+      });
+    }
+
+    const newTeam = new Team({
+      teamName,
+      teamNameLower,
+      members,
+      topic
+    });
+
+    await newTeam.save();
+
+    res.status(201).json({ message: "Team registered successfully", topic });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT} ✅✅✅`);
+})
+
---
+++ server/models/Team.js
@@ -0,0 +1,12 @@
+// models/Team.js
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+  teamName: { type: String, required: true },
+  teamNameLower: { type: String, required: true, unique: true },
+  members: [String],
+  topic: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Team', teamSchema);
